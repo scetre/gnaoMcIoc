5c511bdf92bd3cc494bb3fc089ac44b12f74068b
GNAO <gnao@localhost.localdomain>
2024-09-13 14:59:26 +0200

    creation

---
+++ iocBoot/iocgnaoMc/st.cmd
@@ -0,0 +1,40 @@
+#!../../bin/linux-x86_64/gnaoMc
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/gnaoMc.dbd"
+gnaoMc_registerRecordDeviceDriver pdbbase
+
+# Set local AMS net ID
+AdsSetLocalAMSNetID("192.168.200.7.1.1")
+
+# gnaoMc PLC program connection parameters
+epicsEnvSet("PREFIX_ADSGNAOMC", "ADS-GNAOMC")
+epicsEnvSet("PORT_ADSGNAOMC", "ads-gnaoMc-port")
+epicsEnvSet("IP_ADSGNAOMC", "192.168.206.5")
+epicsEnvSet("AMS_ID_ADSGNAOMC", "192.168.206.1.1")
+
+## Load record instances
+dbLoadRecords("db/gnaoMc.db","P=$(PREFIX_ADSGNAOMC), PORT=$(PORT_ADSGNAOMC)")
+
+# Open ADS port
+# AdsOpen(port_name,
+#         remote_ip_address,
+#         remote_ams_net_id,
+#         sum_buffer_nelem (default: 500),
+#         ads_timeout (default: 500 ms),
+#         device_ads_port (default: 851),
+#         sum_read_period (default: 1 ms))
+AdsOpen("$(PORT_ADSGNAOMC)", "$(IP_ADSGNAOMC)", "$(AMS_ID_ADSGNAOMC)")
+
+# Enable asyn trace output for errors and warnings
+asynSetTraceMask("$(PORT_ADSGNAOMC)", 0, 0x21)
+# Alternatively, output everything
+#asynSetTraceMask("$(PORT_ADSGNAOMC)", 0, 0xff)
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
