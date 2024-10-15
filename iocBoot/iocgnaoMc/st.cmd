2f0ced1c9ab1f6e4b8d48303e96753ed0e403ae1
GNAO <gnao@localhost.localdomain>
2024-10-15 16:46:37 +0200

    updated teh motor interfaces

--- iocBoot/iocgnaoMc/st.cmd
+++ iocBoot/iocgnaoMc/st.cmd
@@ -15,7 +15,7 @@ dbLoadDatabase "dbd/gnaoMc.dbd"
 gnaoMc_registerRecordDeviceDriver pdbbase
 
 # Set local AMS net ID
-AdsSetLocalAMSNetID("192.168.200.2.1.1")
+AdsSetLocalAMSNetID("192.168.200.9.1.1")
 
 # gnaoMc PLC program connection parameters
 epicsEnvSet("PREFIX_ADSGNAOMC", "ADS-GNAOMC")
@@ -34,7 +34,7 @@ dbLoadRecords("db/gnaoMc.db","P=$(PREFIX_ADSGNAOMC), PORT=$(PORT_ADSGNAOMC)")
 #         ads_timeout (default: 500 ms),
 #         device_ads_port (default: 851),
 #         sum_read_period (default: 1 ms))
-AdsOpen("$(PORT_ADSGNAOMC)", "$(IP_ADSGNAOMC)", "$(AMS_ID_ADSGNAOMC)")
+AdsOpen("$(PORT_ADSGNAOMC)", "$(IP_ADSGNAOMC)", "$(AMS_ID_ADSGNAOMC)", 1000, 1000, 851, 10)
 
 # Enable asyn trace output for errors and warnings
 asynSetTraceMask("$(PORT_ADSGNAOMC)", 0, 0x21)
